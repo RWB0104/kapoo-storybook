75fd0e64242502e51d31f698997b2263d922df84
RWB0104 <psj2716@gmail.com>
2025-03-22 12:37:07 +0000

    deploy: 🚀 style: 💄 Ubuntu Mono 폰트 추가

---
+++ docs/global-ui-pack/assets/MarkdownTooltipA.stories-CLKcyxeo.js
@@ -0,0 +1,6 @@
+import"./index-r57MSJFk.js";import{g as p,a as t,S as s}from"./storybook-hTxjENzG.js";import{M as i}from"./MarkdownTooltipA-PbN8F7gz.js";import"./jsx-runtime-BnHmloXs.js";import"./jsx-runtime-BEh3FTX5.js";import"./index-frqvKg3v.js";import"./index-Bx25MwHp.js";import"./index-LHXsjL4R.js";import"./index-DmhnNIJ0.js";import"./utils-jAU0Cazi.js";import"./MarkdownA-CZUtreVJ.js";import"./link-B6xMkqIL.js";import"./router-context.shared-runtime-DTbBUFFY.js";import"./add-base-path-B3GOeE0G.js";import"./use-merged-ref-Cp_uXRvQ.js";import"./Typography-D6IL6RvA.js";import"./createLucideIcon-rzbM5Zb5.js";const r="MarkdownTooltipA",N={argTypes:{children:t("children",r),className:t("className",r),href:{description:"링크",table:{category:r,subcategory:s.PROP}},id:t("id",r)},component:i,parameters:p("마크다운 툴팁 a 태그 컴포넌트",["atom의 `MarkdownA` 컴포넌트와 shadcn-ui의 `Tooltip` 컴포넌트를 결합한 컴포넌트입니다.","호버 시 링크된 URL를 표시할 수 있습니다."]),tags:["autodocs"],title:"molecule/MarkdownTooltipA"},o={args:{children:"Lorem Ipsum",href:"/page/1"}};var e,a,m;o.parameters={...o.parameters,docs:{...(e=o.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    children: 'Lorem Ipsum',
+    href: '/page/1'
+  }
+}`,...(m=(a=o.parameters)==null?void 0:a.docs)==null?void 0:m.source}}};const S=["Playground"];export{o as Playground,S as __namedExportsOrder,N as default};
