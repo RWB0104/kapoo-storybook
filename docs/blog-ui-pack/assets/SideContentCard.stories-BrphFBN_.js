e00e3208d8478f42aba4da32b3408afc5bc47956
RWB0104 <psj2716@gmail.com>
2025-04-01 07:36:06 +0000

    deploy: 🚀 fix: 🩹 레거시 프로퍼티 제거

--- docs/blog-ui-pack/assets/SideContentCard.stories-BrphFBN_.js
+++ docs/blog-ui-pack/assets/SideContentCard.stories-BrphFBN_.js
@@ -1,4 +1,4 @@
-import{j as e}from"./jsx-runtime-BnHmloXs.js";import"./index-CKMdfnwt.js";import{g as I,S as a}from"./storybook-CWpip0ZO.js";import{S as c}from"./StorybookTemplate-h4nRBuW0.js";import{L as N,T as m,n as R}from"./Typography-DPKB2Aff.js";import{c as u}from"./utils-jAU0Cazi.js";import{C as _,a as z}from"./circle-arrow-right-CXU6_5U5.js";import"./jsx-runtime-BEh3FTX5.js";import"./segment-CIEV7OGa.js";import"./index-BY638psU.js";import"./add-base-path-Br4_xqsT.js";function r({title:t,thumbnail:P,direction:o="left",...D}){return e.jsx(N,{"data-component":"SideContentCard",...D,children:e.jsxs("div",{className:"group relative size-full overflow-hidden rounded shadow",children:[e.jsxs("div",{className:u("flex relative z-10 text-white bg-black/50 size-full rounded items-center gap-6 p-6",{"flex-row-reverse":o==="right"}),children:[e.jsxs("div",{className:"shrink-0",children:[o==="left"&&e.jsx(_,{size:40}),o==="right"&&e.jsx(z,{size:40})]}),e.jsxs("div",{className:u("flex flex-col gap-1",{"items-end":o==="right"}),children:[e.jsxs(m,{className:"text-amber-400",variant:"small",children:[o==="left"&&"이전 게시글",o==="right"&&"다음 게시글"]}),e.jsx(m,{children:t})]})]}),e.jsx(R,{alt:t,className:"duration-300 group-hover:scale-110",objectFit:"cover",src:P,fill:!0})]})})}r.__docgenInfo={description:`사이드 컨텐츠 카드 molecule 컴포넌트 반환 메서드
+import{j as e}from"./jsx-runtime-BnHmloXs.js";import"./index-CKMdfnwt.js";import{g as I,S as a}from"./storybook-CWpip0ZO.js";import{S as c}from"./StorybookTemplate-h4nRBuW0.js";import{L as N,T as m,n as R}from"./Typography-DPKB2Aff.js";import{c as u}from"./utils-jAU0Cazi.js";import{C as _,a as z}from"./circle-arrow-right-CXU6_5U5.js";import"./jsx-runtime-BEh3FTX5.js";import"./segment-CIEV7OGa.js";import"./index-BY638psU.js";import"./add-base-path-Br4_xqsT.js";function r({title:t,thumbnail:P,direction:o="left",...D}){return e.jsx(N,{"data-component":"SideContentCard",...D,children:e.jsxs("div",{className:"group relative size-full overflow-hidden rounded shadow",children:[e.jsxs("div",{className:u("flex relative z-10 text-white bg-black/50 size-full rounded items-center gap-6 p-6",{"flex-row-reverse":o==="right"}),children:[e.jsxs("div",{className:"shrink-0",children:[o==="left"&&e.jsx(_,{size:40}),o==="right"&&e.jsx(z,{size:40})]}),e.jsxs("div",{className:u("flex flex-col gap-1",{"items-end":o==="right"}),children:[e.jsxs(m,{className:"text-amber-400",variant:"small",children:[o==="left"&&"이전 게시글",o==="right"&&"다음 게시글"]}),e.jsx(m,{children:t})]})]}),e.jsx(R,{alt:t,className:"object-cover duration-300 group-hover:scale-110",src:P,fill:!0})]})})}r.__docgenInfo={description:`사이드 컨텐츠 카드 molecule 컴포넌트 반환 메서드
 
 @param {SideContentCardProps} param0: SideContentCardProps
 
@@ -16,7 +16,7 @@ import{j as e}from"./jsx-runtime-BnHmloXs.js";import"./index-CKMdfnwt.js";import
             <SideContentCard {...props} />
             <SideContentCard {...props} direction="right" />
         </StorybookTemplate>
-}`,...(x=(C=l.parameters)==null?void 0:C.docs)==null?void 0:x.source}}};var y,S,F;i.parameters={...i.parameters,docs:{...(y=i.parameters)==null?void 0:y.docs,source:{originalSource:`{
+}`,...(x=(C=l.parameters)==null?void 0:C.docs)==null?void 0:x.source}}};var y,S,T;i.parameters={...i.parameters,docs:{...(y=i.parameters)==null?void 0:y.docs,source:{originalSource:`{
   argTypes: {
     thumbnail: {
       control: {
@@ -29,7 +29,7 @@ import{j as e}from"./jsx-runtime-BnHmloXs.js";import"./index-CKMdfnwt.js";import
             <SideContentCard {...props} thumbnail="https://placehold.co/600/FFFFFF/32CD32?font=raleway&text=Lorem+Ipsum" />
             <SideContentCard {...props} thumbnail="https://placehold.co/600/32CD32/000000?font=raleway&text=Lorem+Ipsum" />
         </StorybookTemplate>
-}`,...(F=(S=i.parameters)==null?void 0:S.docs)==null?void 0:F.source}}};var T,j,k;d.parameters={...d.parameters,docs:{...(T=d.parameters)==null?void 0:T.docs,source:{originalSource:`{
+}`,...(T=(S=i.parameters)==null?void 0:S.docs)==null?void 0:T.source}}};var j,F,k;d.parameters={...d.parameters,docs:{...(j=d.parameters)==null?void 0:j.docs,source:{originalSource:`{
   argTypes: {
     title: {
       control: {
@@ -42,7 +42,7 @@ import{j as e}from"./jsx-runtime-BnHmloXs.js";import"./index-CKMdfnwt.js";import
             <SideContentCard {...props} title="Storybook" />
             <SideContentCard {...props} title="Next.js" />
         </StorybookTemplate>
-}`,...(k=(j=d.parameters)==null?void 0:j.docs)==null?void 0:k.source}}};var v,w,L;p.parameters={...p.parameters,docs:{...(v=p.parameters)==null?void 0:v.docs,source:{originalSource:`{
+}`,...(k=(F=d.parameters)==null?void 0:F.docs)==null?void 0:k.source}}};var v,w,L;p.parameters={...p.parameters,docs:{...(v=p.parameters)==null?void 0:v.docs,source:{originalSource:`{
   argTypes: {
     href: {
       control: {
